37b7e1bf8f5563c838f5dd7475d9dc3443b9b74b
Shivakant6387 <shivakant5875@gmail.com>
2022-12-08 11:01:54 +0530

    Added Default property

--- src/main/java/com/example/RestAPIStartDay2/Controller/RestEndPoint.java
+++ src/main/java/com/example/RestAPIStartDay2/Controller/RestEndPoint.java
@@ -1,9 +1,20 @@
 package com.example.RestAPIStartDay2.Controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class RestEndPoint {
+
+    @Value("${default.course.name}")
+    private String CName;
+    @Value("${default.course.chapterCount}")
+    private int chaptersCount;
+    @RequestMapping("/DefaultCourse")
+    public Course getDefaultCourse(@RequestParam(value = "name",defaultValue = " Spring Boot",required = false)String name,
+                              @RequestParam(value = "chapCount",defaultValue = "3",required = false)int chapterCount){
+        return new Course(CName,chaptersCount);
+    }
     @RequestMapping("/course")
 public Course getEndPoint(@RequestParam(value = "name",defaultValue = " Spring Boot",required = false)String name,
                           @RequestParam(value = "chapCount",defaultValue = "3",required = false)int chapterCount){
