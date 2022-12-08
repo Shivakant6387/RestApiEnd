392f8a93bed89b6f4cd9481fada5ba172cebd0fc
Shivakant6387 <shivakant5875@gmail.com>
2022-12-08 10:41:11 +0530

    Added post man rest Api

--- src/main/java/com/example/RestAPIStartDay2/Controller/RestEndPoint.java
+++ src/main/java/com/example/RestAPIStartDay2/Controller/RestEndPoint.java
@@ -1,8 +1,6 @@
 package com.example.RestAPIStartDay2.Controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class RestEndPoint {
@@ -10,5 +8,9 @@ public class RestEndPoint {
 public Course getEndPoint(@RequestParam(value = "name",defaultValue = " Spring Boot",required = false)String name,
                           @RequestParam(value = "chapCount",defaultValue = "3",required = false)int chapterCount){
     return new Course(name,chapterCount);
+}
+@RequestMapping(method = RequestMethod.POST,value = "/register/course")
+public String saveCourse(@RequestBody Course course){
+        return "Your course named"+course.getName()+"with"+course.getChapterCount()+"chapter saved successfully";
 }
 }
